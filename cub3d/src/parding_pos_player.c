e7bc4228f3d6d09384d95a25f74af413bc59272e
BaWiiXx <138687056+BaWiiXx@users.noreply.github.com>
2024-10-16 11:05:55 +0200

    Update parsing_pos_player.c

--- cub3d/src/parding_pos_player.c
+++ cub3d/src/parding_pos_player.c
@@ -14,5 +14,24 @@
 
 void get_pos_player(t_game *pos)
 {
-    ;
+    int x, y;
+
+    y = 0;
+    while (game->map[y])
+    {
+        x = 0;
+        while (game->map[y][x])
+        {
+            if (ft_strchr("NSEW", game->map[y][x]))
+            {
+                game->player.pos_x = x;
+                game->player.pos_y = y;
+                game->player.direction = game->map[y][x];
+                return;
+            }
+            x++;
+        }
+        y++;
+    }
+    exit_error("Error: No player position found\n");
 }
